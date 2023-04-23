66a7a053900ce41e95d153d46c6584c781960f2d
sf2020-LATTE <second.s.fujii@gmail.com>
2023-04-23 10:22:04 +0900

    マス目表示

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -1,10 +1,53 @@
 <script lang="ts" setup>
+const row =  20;
+const colomn =  10;
+
+const field = new Array(row);
+
+for (let i = 0; i < row; i++) {
+    const fieldColumn = new Array(colomn).fill(0);
+    field[i] = fieldColumn;
+}
+field[0][0] = 1;
+field[1][0] = 1;
+field[2][0] = 1;
+field[3][0] = 1;
+
 </script>
 
 <template>
   <h1>プレイ画面</h1>
   <h2>ユーザ名: {{ $route.query.name }}</h2>
+  <div class = "container">
+    <table class="field" style = "border-collapse: collapse">
+      <tr
+        v-for="(row, y) in field"
+        :key = "y">
+        <td
+          class="block"
+          v-for="(col, x) in row"
+          :key="()=>`${x}${y}`"
+        >{{ col }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+}
+
+.field {
+  border: ridge 0.4em #2c3e50;
+  border-top: none;
+}
+
+.block {
+  width: 1em;
+  height: 1em;
+  border: 0.1px solid #95a5a6;
+}
 </style>
